6e517b17515731824e6a2b9448d819f3ec4510c9
crnihr <crni@hi.htnet.hr>
2019-08-16 23:07:22 +0200

    Add files via upload

---
+++ polybar/scripts/compton.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+case "$1" in
+    --toggle)
+        if [ "$(pgrep -x compton)" ]; then
+            pkill compton
+        else
+            compton -b --config ~/.config/compton.conf
+        fi
+        ;;
+    *)
+        if [ "$(pgrep -x compton)" ]; then
+            echo "#1"
+        else
+            echo "#2"
+        fi
+        ;;
+esac
+
