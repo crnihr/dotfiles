375ef7b7c0429e37ef06ee14a9b82ce4ca2bc439
crnihr <crni@hi.htnet.hr>
2019-08-26 19:59:20 +0200

    Add files via upload

--- winfuncs.sh
+++ winfuncs.sh
@@ -7,15 +7,15 @@
 # cascade also shaded windows
 
 # set desktop dimensions
-display_width=$(xdotool getdisplaygeometry | awk '{ print $1 }')
-display_height=$(xdotool getdisplaygeometry | awk '{ print $2 }')
+display_width=$(xdotool getdisplaygeometry | cut -d" " -f1)
+display_height=$(xdotool getdisplaygeometry | cut -d" " -f2)
 
-# window decorations (not used)
+# window decorations
 window_id=$(xdotool getactivewindow)
-y_offset=$(xwininfo -id "$window_id" | awk '/Relative upper-left Y:/ { print $4 }')
+titlebar_offset=$(xwininfo -id "$window_id" | awk '/Relative upper-left Y:/ { print $4 }')
 
 # top panel
-top_bar=$(xprop -root _NET_WORKAREA | awk '{ print $4 }' | colrm 3)
+top_bar=$(xprop -root _NET_WORKAREA | awk '{ print $4 }' | cut -d"," -f1)
 
 
 function get_desktop_dim {	
@@ -69,10 +69,10 @@ function win_tile_two {
 	is_desktop "$wid2" && return
 
 	half_w=`expr ${DIM[0]} / 2`
-	win_h=`expr ${DIM[1]} - $top_bar`
+	win_h=${DIM[1]}
 
-	commands="windowsize $wid1 $half_w $win_h"
-	commands="$commands windowsize $wid2 $half_w $win_h"
+	commands="windowsize $wid1 $half_w `expr $win_h - $titlebar_offset`"
+	commands="$commands windowsize $wid2 $half_w `expr $win_h - $titlebar_offset`"
 	commands="$commands windowmove $wid1 0 $top_bar"
 	commands="$commands windowmove $wid2 $half_w $top_bar"
 	commands="$commands windowraise $wid1"
@@ -80,6 +80,105 @@ function win_tile_two {
 
 	wmctrl -i -r $wid1 -b remove,maximized_vert,maximized_horz
 	wmctrl -i -r $wid2 -b remove,maximized_vert,maximized_horz
+
+	echo "$commands" | xdotool -
+}
+
+function win_stack_two {
+	get_desktop_dim
+
+	wid1=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid1" && return
+
+	wid2=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid2" && return
+
+	win_w=${DIM[0]}
+        half_h=`expr ${DIM[1]} / 2`
+
+	commands="windowsize $wid1 $win_w `expr $half_h - $titlebar_offset`"
+	commands="$commands windowsize $wid2 $win_w `expr $half_h - $titlebar_offset`"
+	commands="$commands windowmove $wid1 0 $top_bar"
+	commands="$commands windowmove $wid2 0 `expr $half_h + $top_bar`"
+	commands="$commands windowraise $wid1"
+	commands="$commands windowraise $wid2"
+
+	wmctrl -i -r $wid1 -b remove,maximized_vert,maximized_horz
+	wmctrl -i -r $wid2 -b remove,maximized_vert,maximized_horz
+
+	echo "$commands" | xdotool -
+}
+
+
+function win_tile_three {
+	get_desktop_dim
+
+	wid1=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid1" && return
+
+	wid2=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid2" && return
+	
+	wid3=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid3" && return
+
+        win_h=${DIM[1]}
+        half_w=`expr ${DIM[0]} / 2`
+	half_h=`expr ${win_h} / 2`
+
+	commands="windowsize $wid1 $half_w `expr $win_h - $titlebar_offset`"
+	commands="$commands windowsize $wid2 $half_w `expr $half_h - $titlebar_offset`"
+	commands="$commands windowsize $wid3 $half_w `expr $half_h - $titlebar_offset`"
+	commands="$commands windowmove $wid1 0 $top_bar"
+	commands="$commands windowmove $wid2 $half_w $top_bar"
+	commands="$commands windowmove $wid3 $half_w `expr $half_h + $top_bar`"
+	commands="$commands windowraise $wid1"
+	commands="$commands windowraise $wid2"
+	commands="$commands windowraise $wid3"
+
+	wmctrl -i -r $wid1 -b remove,maximized_vert,maximized_horz
+	wmctrl -i -r $wid2 -b remove,maximized_vert,maximized_horz
+	wmctrl -i -r $wid3 -b remove,maximized_vert,maximized_horz
+
+	echo "$commands" | xdotool -
+}
+
+function win_tile_three_v {
+	get_desktop_dim
+
+	wid1=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid1" && return
+
+	wid2=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid2" && return
+	
+	wid3=`xdotool selectwindow 2>/dev/null`
+
+	is_desktop "$wid3" && return
+
+        win_h=${DIM[1]}
+        half_w=`expr ${DIM[0]} / 3`
+
+	commands="windowsize $wid1 $half_w `expr $win_h - $titlebar_offset`"
+	commands="$commands windowsize $wid2 $half_w `expr $win_h - $titlebar_offset`"
+	commands="$commands windowsize $wid3 $half_w `expr $win_h - $titlebar_offset`"
+	commands="$commands windowmove $wid1 0 $top_bar"
+	commands="$commands windowmove $wid2 $half_w $top_bar"
+	commands="$commands windowmove $wid3 `expr $half_w \* 2` $top_bar"
+	commands="$commands windowraise $wid1"
+	commands="$commands windowraise $wid2"
+	commands="$commands windowraise $wid3"
+
+	wmctrl -i -r $wid1 -b remove,maximized_vert,maximized_horz
+	wmctrl -i -r $wid2 -b remove,maximized_vert,maximized_horz
+	wmctrl -i -r $wid3 -b remove,maximized_vert,maximized_horz
 
 	echo "$commands" | xdotool -
 }
@@ -117,7 +216,7 @@ function win_tile {
 	for window in ${WDOWS[@]} ; do
 		wmctrl -i -r $window -b remove,maximized_vert,maximized_horz
 
-		commands="$commands windowsize $window $win_w `expr $win_h - $top_bar`"
+		commands="$commands windowsize $window $win_w `expr $win_h - $titlebar_offset`"
 		commands="$commands windowmove $window `expr $x \* $win_w` `expr $y \* $win_h + $top_bar`"
 
 		x=`expr $x + 1`
@@ -141,7 +240,7 @@ function win_cascade {
 		wmctrl -i -r $window -b remove,maximized_vert,maximized_horz
 
 		commands="$commands windowsize $window 1024 640"
-		commands="$commands windowmove $window $x $y"
+		commands="$commands windowmove $window $x `expr $y + $top_bar`"
 
 		x=`expr $x + 120`
 	  y=`expr $y + 100`
@@ -198,7 +297,13 @@ for command in ${@} ; do
 		win_select
 	elif [[ "$command" == "tiletwo" ]] ; then
 		win_tile_two
-	elif [[ "$command" == "cascade" ]] ; then
+	elif [[ "$command" == "stacktwo" ]] ; then
+		win_stack_two
+	elif [[ "$command" == "tilethree" ]] ; then
+		win_tile_three
+	elif [[ "$command" == "tilethreev" ]] ; then
+		win_tile_three_v
+        elif [[ "$command" == "cascade" ]] ; then
 		win_cascade
 	elif [[ "$command" == "showdesktop" ]] ; then
 		win_showdesktop
