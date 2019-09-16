f465a6a41d66028714051ca102b8ee22e91e8781
crnihr <crni@hi.htnet.hr>
2019-09-16 16:43:44 +0200

    Add files via upload

--- polybar/scripts/zentile.sh
+++ polybar/scripts/zentile.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 case "$1" in
     --toggle)
         if [ "$(pgrep -x zentile)" ]; then
+            xdotool key "Control_L+Shift_L+u"
+            sleep 2
             killall -q zentile
         else
-            ~/.config/zentile/zentile
+            zentile &
+            sleep 2
+            xdotool key "Control_L+Shift_L+t"
         fi
         ;;
     *)
